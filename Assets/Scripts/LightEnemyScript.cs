bba0e143c8eea7ec601950673be4b1f5707d69ef
PurpleRiggz <purpleriggz@gmail.com>
2023-04-30 15:22:42 -0400

    Added damage, a ranged attack, and checkpoints

--- Assets/Scripts/LightEnemyScript.cs
+++ Assets/Scripts/LightEnemyScript.cs
@@ -6,18 +6,22 @@ public class LightEnemyScript : MonoBehaviour
 {
     // Public
     public float speed;
+    public int health;
     public int currentPatrolPointIndex = 0;
+    public GameObject player;
     public Transform[] patrolPoints;
     public Transform currentPatrolPoint;
 
     // Private
     SpriteRenderer spriteRenderer;
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         currentPatrolPoint = patrolPoints[currentPatrolPointIndex];
         spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
         DirectionSwitch();
     }
 
@@ -25,7 +29,7 @@ public class LightEnemyScript : MonoBehaviour
     void Update()
     {
         transform.Translate(speed * Time.deltaTime * Vector2.right);
-        if(Vector2.Distance(currentPatrolPoint.position, transform.position) < .46f)
+        if (Vector2.Distance(currentPatrolPoint.position, transform.position) < .46f)
         {
             GetNextPatrolPoint();
         }
@@ -58,6 +62,49 @@ public class LightEnemyScript : MonoBehaviour
         {
             speed = 1f;
             spriteRenderer.flipX = true;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Laser"))
+        {
+            anim.SetTrigger("Hit");
+            // Destroy laser
+            Destroy(other.gameObject);
+            health -= 5;
+            Death();
+        }
+        if (other.CompareTag("Sword"))
+        {
+            anim.SetTrigger("Hit");
+            health -= 5;
+            Invoke(nameof(Death), .2f);
+        }
+    }
+
+    void KnockBack()
+    {
+        float distanceFromPlayer = transform.position.x - player.transform.position.x;
+
+        if (distanceFromPlayer > 0)
+        {
+            spriteRenderer.flipX = false;
+            speed = 4.5f;
+        }
+        else
+        {
+            spriteRenderer.flipX = true;
+            speed = -4.5f;
+        }
+    }
+
+    void Death()
+    {
+        if (health <= 0)
+        {
+            // Destroy enemy
+            Destroy(gameObject);
         }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,25 +5,44 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    // Variables
     // Public
     public float speed;
     public float jumpForce;
     public float attackDelay;
+    public int health;
     public bool airControl;
-    public Text messageText;
 
     // Private
     float horizontal = 0f;
     float lastAttackTime;
+    bool facingRight;
+    bool disabled;
 
     // Class Objects
+    // Public
+    public GameObject laserBeam;
+    public GameObject swordRt;
+    public GameObject swordLt;
+    public Transform laserPositionRt;
+    public Transform laserPositionLt;
+    public Transform currentCheckpoint;
+    public Text messageText;
+    public RuntimeAnimatorController unarmedController;
+    public RuntimeAnimatorController swordController;
+    public RuntimeAnimatorController gunController;
+
+    // Private
     Rigidbody2D rb;
     SpriteRenderer spriteRenderer;
     Animator anim;
 
+
     // Start is called before the first frame update
     void Start()
     {
+        facingRight = true;
+        disabled = false;
         rb = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
@@ -32,6 +51,12 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (disabled)
+        {
+            return;
+        }
+
         // AirControl
         if(airControl)
         {
@@ -50,13 +75,15 @@ public class Player : MonoBehaviour
         transform.Translate(horizontal * speed * Time.deltaTime * Vector2.right);
 
         // Flip the sprite to face the direction he is moving
-        if (horizontal > 0f)
+        if (horizontal > 0.01f)
         {
             spriteRenderer.flipX = false;
+            facingRight = true;
         }
-        else if (horizontal < 0f)
+        else if (horizontal < -0.01f)
         {
             spriteRenderer.flipX = true;
+            facingRight = false;
         }
 
         // Set the speed parameter of the animator
@@ -70,7 +97,7 @@ public class Player : MonoBehaviour
         }
 
         // Tells the animator to start the jumping animation
-        if (!anim.GetBool("Jumping") && Mathf.Abs(rb.velocity.y) > .05)
+        if (!anim.GetBool("Jumping") && Mathf.Abs(rb.velocity.y) > .5)
         {
             anim.SetBool("Jumping", true);
         }
@@ -86,11 +113,67 @@ public class Player : MonoBehaviour
             anim.SetTrigger("Attack");
             lastAttackTime = Time.time;
         }
+
+        // Weapon switching
+        if (Input.GetKeyDown("u"))
+        {
+            // Switch to unarmed controller
+            anim.runtimeAnimatorController = unarmedController as RuntimeAnimatorController;
+            attackDelay = 2;
+        }
+        if (Input.GetKeyDown("g"))
+        {
+            // Switch to gun controller
+            anim.runtimeAnimatorController = gunController as RuntimeAnimatorController;
+            attackDelay = 0.7f;
+        }
+        if (Input.GetKeyDown("m"))
+        {
+            // Switch to unarmed controller
+            anim.runtimeAnimatorController = swordController as RuntimeAnimatorController;
+            attackDelay = 0.2191338f;
+        }
     }
 
     void UnarmedAttack()
     {
         DisplayMessage("I am unarmed and cannot attack...", 2.0f);
+    }
+
+    void SwordAttack()
+    {
+        if (facingRight)
+        {
+            swordRt.SetActive(true);
+        }
+        else
+        {
+            swordLt.SetActive(true);
+        }
+    }
+
+    void SwordAttackDone()
+    {
+        if (facingRight)
+        {
+            swordRt.SetActive(false);
+        }
+        else
+        {
+            swordLt.SetActive(false);
+        }
+    }
+
+    void LaserAttack()
+    {
+        if(facingRight)
+        {
+            Instantiate(laserBeam,laserPositionRt.position, transform.rotation);
+        }
+        else
+        {
+            Instantiate(laserBeam,laserPositionLt.position, Quaternion.Euler(0f,0f,180f));
+        }
     }
 
     void DisplayMessage(string message, float displayTime)
@@ -102,5 +185,45 @@ public class Player : MonoBehaviour
     void ClearMessage()
     {
         messageText.text = string.Empty;
+    }
+    void CheckDeath()
+    {
+        if (health <= 0)
+        {
+            spriteRenderer.enabled = false;
+            disabled = true;
+            Invoke(nameof(Respawn), 2f);
+        }
+    }
+
+    void Respawn()
+    {
+        transform.position = currentCheckpoint.position;
+        spriteRenderer.enabled = true;
+        disabled = false;
+        health = 25;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            health -= 5;
+            CheckDeath();
+        }
+        if (collision.gameObject.CompareTag("FallDeath"))
+        {
+            health -= 1000;
+            CheckDeath();
+        }
+    }
+
+    // This function will run whenever the player collides with a trigger collider
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Checkpoint"))
+        {
+            currentCheckpoint = collision.transform;
+        }
     }
 }
