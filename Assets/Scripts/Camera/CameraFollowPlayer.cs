bafeacc7779d0fc85104de2d729ced9c7c806ab7
PurpleRiggz <purpleriggz@gmail.com>
2023-05-06 17:54:19 -0400

    Reverted some stuff to fix what I broke. Fixed a piece of foreground that was supposed to be background

--- Assets/Scripts/Camera/CameraFollowPlayer.cs
+++ Assets/Scripts/Camera/CameraFollowPlayer.cs
@@ -7,14 +7,26 @@ using UnityEngine.SceneManagement;
 public class CameraFollowPlayer : MonoBehaviour
 {
     private CinemachineVirtualCamera cinemachineVirtualCamera;
+    private CinemachineConfiner cinemachineConfiner;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         cinemachineVirtualCamera = GetComponent<CinemachineVirtualCamera>();
+        cinemachineConfiner = GetComponent<CinemachineConfiner>();
         if (SceneManager.GetActiveScene() != SceneManager.GetSceneByName("Main Menu"))
         {
             cinemachineVirtualCamera.Follow = GameObject.FindGameObjectWithTag("Player").transform;
+            cinemachineConfiner.m_BoundingShape2D = GameObject.FindGameObjectWithTag("CameraStop").GetComponent<PolygonCollider2D>();
+        }
+    }
+
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene() != SceneManager.GetSceneByName("Main Menu"))
+        {
+            cinemachineVirtualCamera.Follow = GameObject.FindGameObjectWithTag("Player").transform;
+            cinemachineConfiner.m_BoundingShape2D = GameObject.FindGameObjectWithTag("CameraStop").GetComponent<PolygonCollider2D>();
         }
     }
 }
